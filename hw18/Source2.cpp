a7bc8b5fa0fcbf0938c4d56276233ab16dfd062c
Denis Eliseev <125156318+Denelis0@users.noreply.github.com>
2025-04-03 14:01:09 +0300

    Update Source2.cpp

--- hw18/Source2.cpp
+++ hw18/Source2.cpp
@@ -18,20 +18,20 @@ int main() {
     line(inputImage, Point(50, 50), Point(200, 50), Scalar(0, 255, 0), 2);
 
     // Рисуем прямоугольник
-    rectangle(inputImage, Point(50, 80), Point(200, 150), Scalar(255, 0, 0), 2);
+    rectangle(inputImage, Point(400, 80), Point(200, 150), Scalar(255, 0, 0), 2);
 
     // Рисуем круг
-    circle(inputImage, Point(125, 250), 50, Scalar(0, 0, 255), 2);
+    circle(inputImage, Point(300, 250), 50, Scalar(0, 0, 255), 2);
 
     // Рисуем эллипс
-    ellipse(inputImage, Point(125, 350), Size(60, 30), 30, 0, 360, Scalar(255, 255, 0), 2);
+    ellipse(inputImage, Point(200, 350), Size(60, 30), 30, 0, 360, Scalar(255, 255, 0), 2);
 
     // Рисуем многоугольник
     vector<Point> polygonPoints = { Point(50, 400), Point(100, 450), Point(150, 400), Point(100, 350) };
-    polylines(inputImage, polygonPoints, true, Scalar(255, 0, 255), 2);
+    polylines(inputImage, polygonPoints, true, Scalar(100, 3, 255), 2);
 
     // Добавляем текст
-    putText(inputImage, "Arcane", Point(50, 480), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 255), 2);
+    putText(inputImage, "Arcane", Point(500, 400), FONT_HERSHEY_SIMPLEX, 1, Scalar(0, 255, 254), 2);
 
     // Отображаем изображение
     imshow("Modified Image", inputImage);
